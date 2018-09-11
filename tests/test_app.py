f56b56c99c7d87ca29a685484d41e171f4af0386
thej <thej.kodiyat@rackspace.com>
2018-09-11 11:45:13 -0500

    added test_saving_image test case

--- tests/test_app.py
+++ tests/test_app.py
@@ -56,7 +56,7 @@ def test_posted_image_gets_saved(client, monkeypatch):
     assert response.headers['location'] == '/images/{}.png'.format(fake_uuid) # return the images's resource location
 """
 
-# With clever composition of fictures, we can observe what happens with the mock injected into the image resource.
+# With clever composition of fixtures, we can observe what happens with the mock injected into the image resource.
 def test_post_image(client, mock_store):
     file_name = 'fake-image-name.xyz'
 
@@ -74,6 +74,36 @@ def test_post_image(client, mock_store):
     assert response.headers['location'] == '/images/{}'.format(file_name)
     saver_call = mock_store.save.call_args
 
-    # saver_call is a unittest.mock.call tuple. It's first element is a tuple of positional arguments supplied when calling the mock.
+    # saver_call is a unittest.mock.call tuple. It's first element is a tuple of positional arguments supplied when 
+    # calling the mock.
     assert isinstance(saver_call[0][0], falcon.request_helpers.BoundedStream)
     assert saver_call[0][1] == image_content_type
+    # Fewer mocks are used while the assertions have gotten more elaborate to properly check interactions at the 
+    # interface boundaries.
+
+
+# But since we used a mock in the above test, we are no longer covering the actual saving of the image. Below test 
+# will do that.
+def test_saving_image(monkeypatch):
+    # This still has some mocks, but they are more localized and do not have to be monkey-patched into standard 
+    # library modules (always a risky business)
+    mock_file_open = mock_open()
+
+    fake_uuid = '123e4567-e89b-12d3-a456-426655440000'
+    def mock_uuidgen():
+        return fake_uuid
+    
+    fake_image_bytes = b'fake-image-bytes'
+    fake_request_stream = io.BytesIO(fake_image_bytes)
+    storage_path = 'fake-storage-path'
+    store = my_falcon.images.ImageStore(
+        storage_path,
+        uuidgen=mock_uuidgen,
+        fopen=mock_file_open
+    )
+
+    assert store.save(fake_request_stream, 'image/png') == fake_uuid + '.png'
+    assert call().write(fake_image_bytes) in mock_file_open.mock_calls
+    # Like the former tests, this one still uses mocks. But the structure of the code has been improved through the 
+    # techniques of componentization and dependency inversion, making the the application more flexible and testable.
+
